771666803afebd2dd3690527281be1b4d84b8b7e
Greissy Naranjo <greissynv@gmail.com>
2025-01-08 14:44:01 +0100

    final

--- main.js
+++ main.js
@@ -22,9 +22,9 @@ randomize.addEventListener("click", result);
 function result() {
     let newStory = storyText;
   
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
+    const xItem = randomValueFromArray(insertx);
+    const yItem = randomValueFromArray(inserty);
+    const zItem = randomValueFromArray(insertz);
   
     newStory = newStory.replaceAll(":insertx:",xItem);
     newStory = newStory.replaceAll(":inserty:",yItem);
